983c3e568d5b30eda31eed4af8a190580860773c
tilana <nataliewidmann@gmx.de>
2017-09-04 12:49:21 +0200

    add logging to see accuracy results printed in the console

--- newsAnalysis/evaluateModel.py
+++ newsAnalysis/evaluateModel.py
@@ -1,14 +1,16 @@
 from gensim.models import Word2Vec 
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 import pdb
 
 
 def evaluateModel():
 
     evaluationFile = '../questions-words.txt'
-    w2vPath = 'models/w2v'
+    w2vPath = 'models/w2v_5000'
 
     word2vec = Word2Vec.load(w2vPath)
-    accuracy = word2vec.accuracy(evaluationFile, restrict_vocab=100000)
+    accuracy = word2vec.wv.accuracy(evaluationFile, restrict_vocab=300000)
 
 
     pdb.set_trace() 
