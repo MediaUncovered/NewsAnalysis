5852beb4e0fe19b28ffd1554115bdbf4312251ba
tilana <nataliewidmann@gmx.de>
2017-09-04 12:48:01 +0200

    separate the word2vec model generation into two steps: build_vocab and train to avoid issues with the generator object and keep code efficient

--- newsAnalysis/script.py
+++ newsAnalysis/script.py
@@ -3,6 +3,7 @@ from sys import argv
 from Sentences import Sentences
 import gensim, logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+import pdb
 
 
 DATABASE = argv[1]
@@ -10,6 +11,8 @@ HOST = argv[2]
 PORT = argv[3]
 USER = argv[4]
 PASSWORD = argv[5]
+
+nrDocs = 5000
 
 
 def script():
@@ -21,11 +24,18 @@ def script():
     sentences.connectDatabase(info)
     
     print 'Query database'
-    sentences.queryDatabase(limit=10000)
+    sentences.queryDatabase(limit=nrDocs)
 
-    print 'Word2Vec Model' 
-    w2v_model = gensim.models.Word2Vec(sentences=sentences, min_count=10, window=5, workers=4, size=300)
-    w2v_model.save('models/w2v')
+    print 'Word2Vec Model - Build Vocab' 
+    w2v_model = gensim.models.Word2Vec(min_count=8, window=5, workers=3, size=300, alpha=0.05, negative=10)
+    w2v_model.build_vocab(sentences)
+
+
+    print 'Word2Vec Model - Train Model'  
+    sentences.queryDatabase(limit=nrDocs, cursorname='test2')
+    w2v_model.train(sentences, total_examples=w2v_model.corpus_count, epochs=w2v_model.iter)
+
+    w2v_model.save('models/w2v_%i' % nrDocs)
 
 
 
