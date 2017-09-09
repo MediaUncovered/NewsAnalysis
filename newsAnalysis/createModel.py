7fe2b33c1babdde71315e3c0e9bcf215cff2eea4
tilana <nataliewidmann@gmx.de>
2017-09-09 15:11:15 +0200

    use number of documents as input arguments to create and evaluate the models

--- newsAnalysis/createModel.py
+++ newsAnalysis/createModel.py
@@ -1,46 +1,45 @@
-from Info import Info 
+from Info import Info
 from sys import argv
 from Sentences import Sentences
 import gensim, logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-import pdb
 
 
-DATABASE = argv[1]
-HOST = argv[2]
-PORT = argv[3]
-USER = argv[4]
-PASSWORD = argv[5]
-
-nrDocs = 5000
+DATABASE = argv[2]
+HOST = argv[3]
+PORT = argv[4]
+USER = argv[5]
+PASSWORD = argv[6]
 
 
-def script():
+def createModel(nrDocs):
 
     info = Info()
     info.database(DATABASE, HOST, PORT, USER, PASSWORD)
 
     sentences = Sentences()
     sentences.connectDatabase(info)
-    
+
     print 'Query database'
     sentences.queryDatabase(limit=nrDocs)
 
-    print 'Word2Vec Model - Build Vocab' 
-    w2v_model = gensim.models.Word2Vec(min_count=8, window=5, workers=3, size=300, alpha=0.05, negative=10)
+    print 'Word2Vec Model - Build Vocab'
+    w2v_model = gensim.models.Word2Vec(min_count=8, window=5, workers=4, size=300, alpha=0.05, negative=10)
     w2v_model.build_vocab(sentences)
 
 
-    print 'Word2Vec Model - Train Model'  
+    print 'Word2Vec Model - Train Model'
     sentences.queryDatabase(limit=nrDocs, cursorname='test2')
     w2v_model.train(sentences, total_examples=w2v_model.corpus_count, epochs=w2v_model.iter)
 
-    w2v_model.save('models/w2v_%i' % nrDocs)
+    path = 'models/w2v_%i' % nrDocs
+    w2v_model.save(path)
 
 
 
 if __name__=='__main__':
-    script()
+    nrDocs = int(argv[1])
+    createModel(nrDocs)
 
 
-    
+
--- newsAnalysis/evaluateModel.py
+++ newsAnalysis/evaluateModel.py
@@ -1,20 +1,18 @@
-from gensim.models import Word2Vec 
+from gensim.models import Word2Vec
+import sys
 import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-import pdb
 
 
-def evaluateModel():
+def evaluateModel(nrDocs):
 
-    evaluationFile = '../questions-words.txt'
-    w2vPath = 'models/w2v_5000'
+    evaluationFile = 'questions-words.txt'
+    w2vPath = 'models/w2v_%i' % nrDocs
 
     word2vec = Word2Vec.load(w2vPath)
-    accuracy = word2vec.wv.accuracy(evaluationFile, restrict_vocab=300000)
-
-
-    pdb.set_trace() 
+    accuracy = word2vec.wv.accuracy(evaluationFile)
 
 
 if __name__=='__main__':
-    evaluateModel()
+    nrDocs = int(sys.argv[1])
+    evaluateModel(nrDocs)
