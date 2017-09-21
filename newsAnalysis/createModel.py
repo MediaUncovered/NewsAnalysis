0ff04b504e3e3ad145188e5022995cfa21032cf4
tilana <nataliewidmann@gmx.de>
2017-09-21 10:01:29 +0200

    option to save model to binary file

--- newsAnalysis/createModel.py
+++ newsAnalysis/createModel.py
@@ -11,7 +11,16 @@ def createModel(file_path, output_file_path):
 
     print('Word2Vec Model - Build Vocab')
     w2v_model = gensim.models.Word2Vec(min_count=8, window=5, workers=4, size=300, alpha=0.05, negative=10)
-    w2v_model.build_vocab(sentences.open(file_path))
+    w2v_model.build_vocab(sentences)
+
+
+    print 'Word2Vec Model - Train Model'
+    sentences.queryDatabase(limit=nrDocs, cursorname='test2')
+    w2v_model.train(sentences, total_examples=w2v_model.corpus_count, epochs=w2v_model.iter)
+
+    path = 'models/w2v_%i' % nrDocs
+    w2v_model.save(path)
+    #word2vec.wv.save(path + '.bin', binary=True)
 
     print('Word2Vec Model - Train Model')
     w2v_model.train(sentences.open(file_path), total_examples=w2v_model.corpus_count, epochs=w2v_model.iter)
