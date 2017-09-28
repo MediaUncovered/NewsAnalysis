d449da93382acadf369736967090fd671e553400
tilana <nataliewidmann@gmx.de>
2017-09-28 09:39:06 +0200

    change name of datastore module, add doc limit

--- newsAnalysis/createDatabase.py
+++ newsAnalysis/createDatabase.py
@@ -1,6 +1,6 @@
 from sys import argv
-from data import model
-from data import collect
+from datastore import model
+from datastore import collect
 
 DATABASE = argv[1]
 HOST = argv[2]
@@ -10,10 +10,13 @@ PASSWORD = argv[5]
 
 def createDatabase():
 
+    path = 'articles3.csv'
+    doc_limit = 120
+
     engine = model.engine(database_user=DATABASE, database_password=PASSWORD, database_host=HOST, database_port=PORT, database_name=DATABASE)
 
     session = model.session(engine)
-    collect.download_articles_to_file(session, "articles.csv")
+    collect.download_articles_to_file(session, source_id=1, limit=doc_limit)
 
 
 if __name__=='__main__':
