36e7db07e3b8b71f6e16c2117d818f927b8d31e3
tilana <NatalieWidmann@gmx.de>
2018-08-28 21:49:29 +0200

    add model type before loading a model to ensure that path to stored model is found

--- run.py
+++ run.py
@@ -4,20 +4,25 @@ from newsAnalysis.Model import Model
 from newsAnalysis.createDatabase import createDatabase
 
 name = 'Moscow_Times_' + str(config.NO_DOCS)
+model_type = 'fasttext'
+
 data_path ='./data/' + name + '.csv'
+
 
 if not os.path.exists(data_path):
     createDatabase(config.DB, config.HOST, config.PORT, config.USER, config.PASSWORD, data_path, config.NO_DOCS)
 
-model = Model(name, 'fasttext')
+model = Model(name, model_type)
 
 if model.exists():
     model_path = model.model_path
     model = model.load(model_path=model_path)
 else:
-    model.create(data_path, modelName=name, modelType='fasttext')
+    model.create(data_path, modelName=name, modelType=model_type)
     model.evaluate()
     model.save()
 
 similarWords = model.word_embedding.wv.similar_by_word('apple')
 print(similarWords)
+
+path = '../explorer-server/models/model'
